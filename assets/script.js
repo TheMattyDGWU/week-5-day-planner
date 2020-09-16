8521b2684b8a827930353612a231cce02af1d778
TheMattyDGWU <themattydgwu@gmail.com>
2020-09-16 17:15:58 -0400

    fixed jscript loop for rows

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // variable for hours of the day set
+    // Hours of the day variable
     let workHours = [
         "9:00 am",
         "10:00 am",
@@ -12,7 +12,7 @@ $(document).ready(function () {
         "5:00 pm",
     ];
 
-    // display current day
+    // displays current day
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
     // For Loop with JQuery draws row hours with assigned classes and IDs
@@ -24,13 +24,13 @@ $(document).ready(function () {
         $(`<textarea class="col-8 note-input" id="input-${i}"></textarea>`)
             .attr("placeholder", "Enter note here")
             .appendTo(".row");
-        $(`<button class="col-2 btn btn-primary saveBtn" id="hour${i}"></button>`)
+        $(`<button class="col-2 btn btn-secondary saveBtn" id="hour${i}"></button>`)
             .text("save")
             .appendTo(".row");
     }
 
     // Reset button
-    $(`<button class="=btn btn-danger clear"></button>`)
+    $(`<button class="=btn btn-success clear"></button>`)
         .text("Clear Schedule")
         .appendTo(".clear");
 
@@ -43,7 +43,7 @@ $(document).ready(function () {
     // Moment.js to get the current date and time
     let now = new Date().getHours();
 
-    // Determine if each row is past, present, or future (24 hour time-slots).... Need to find a better way
+    // Determine if each row is past, present, or future (24 hour time-slots).... Need to create a better way
     if (now > 9) {
         $("#input-0").addClass("past");
     } else if (now >= 9 && now < 10) {
@@ -116,20 +116,19 @@ $(document).ready(function () {
         $("#input-8").addClass("future");
     }
 
-    // Taking the user data, putting to local storage and saving it to the hour in designated row
-
+    // Input user data, post to local storage, and save in correct time-slot
     // 9am
 
     let row9amInput = document.querySelector("#input-0");
     let row9amSaveBtn = document.querySelector("#hour0");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row9amInput.value = localStorage.getItem("stored0");
 
     // Save button clicks and stores the user input data
     hour0.addEventListener("click", updateOutput1);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function updateOutput1() {
         localStorage.setItem("stored0", row9amInput.value);
     }
@@ -139,13 +138,13 @@ $(document).ready(function () {
     let row10amInput = document.querySelector("#input-1");
     let row10amSaveBtn = document.querySelector("#hour1");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row10amInput.value = localStorage.getItem("stored1");
 
     // Save button clicks and stores the user input data
     hour1.addEventListener("click", userDataStore1);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function userDataStore1() {
         localStorage.setItem("stored1", row10amInput.value);
     }
@@ -155,13 +154,13 @@ $(document).ready(function () {
     let row11amInput = document.querySelector("#input-2");
     let row11amSaveBtn = document.querySelector("#hour2");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row11amInput.value = localStorage.getItem("stored2");
 
     // Save button clicks and stores the user input data
     hour2.addEventListener("click", userDataStore2);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function userDataStore2() {
         localStorage.setItem("stored2", row11amInput.value);
     }
@@ -171,13 +170,13 @@ $(document).ready(function () {
     let row12pmInput = document.querySelector("#input-3");
     let row12pmSaveBtn = document.querySelector("#hour3");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row12pmInput.value = localStorage.getItem("stored3");
 
     // Save button clicks and stores the user input data
     hour3.addEventListener("click", userDataStore3);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function userDataStore3() {
         localStorage.setItem("stored3", row12pmInput.value);
     }
@@ -187,13 +186,13 @@ $(document).ready(function () {
     let row1pmInput = document.querySelector("#input-4");
     let row1pmSaveBtn = document.querySelector("#hour4");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row1pmInput.value = localStorage.getItem("stored4");
 
     // Save button clicks and stores the user input data
     hour4.addEventListener("click", updateOutput4);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function updateOutput4() {
         localStorage.setItem("stored4", row1pmInput.value);
     }
@@ -203,13 +202,13 @@ $(document).ready(function () {
     let row2pmInput = document.querySelector("#input-5");
     let row2pmSaveBtn = document.querySelector("#hour5");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row2pmInput.value = localStorage.getItem("stored5");
 
     // Save button clicks and stores the user input data
     hour5.addEventListener("click", updateOutput5);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function updateOutput5() {
         localStorage.setItem("stored5", row2pmInput.value);
     }
@@ -219,13 +218,13 @@ $(document).ready(function () {
     let row3pmInput = document.querySelector("#input-6");
     let row3pmSaveBtn = document.querySelector("#hour6");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row3pmInput.value = localStorage.getItem("stored6");
 
     // Save button clicks and stores the user input data
     hour6.addEventListener("click", updateOutput6);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function updateOutput6() {
         localStorage.setItem("stored6", row3pmInput.value);
     }
@@ -235,13 +234,13 @@ $(document).ready(function () {
     let row4pmInput = document.querySelector("#input-7");
     let row4pmSaveBtn = document.querySelector("#hour7");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row4pmInput.value = localStorage.getItem("stored7");
 
     // Save button clicks and stores the user input data
     hour7.addEventListener("click", updateOutput7);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function updateOutput7() {
         localStorage.setItem("stored7", row4pmInput.value);
     }
@@ -251,13 +250,13 @@ $(document).ready(function () {
     let row5pmInput = document.querySelector("#input-8");
     let row5pmSaveBtn = document.querySelector("#hour8");
 
-    // Displays any user input data already saved
+    // Display previous user input
     row5pmInput.value = localStorage.getItem("stored8");
 
     // Save button clicks and stores the user input data
     hour8.addEventListener("click", updateOutput8);
 
-    // Saves user input data to Local Storage
+    // Save user input to Local Storage
     function updateOutput8() {
         localStorage.setItem("stored8", row5pmInput.value);
     }
